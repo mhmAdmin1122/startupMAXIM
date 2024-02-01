d8f73cde51610259e56f79bc257afb589c6119f8
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-02-01 22:04:51 +0500

    error removes from

--- pages/Components/S/ServiceCard.tsx
+++ pages/Components/S/ServiceCard.tsx
@@ -3,15 +3,21 @@ import Link from "next/link";
 import React from "react";
 import { FaChevronRight } from "react-icons/fa6";
 
-const ServiceCard = ({imageUrl, title, desc, linkurl}:any) => {
+const ServiceCard = ({ imageUrl, title, desc, linkurl }: any) => {
   return (
-    <div id="card1" className="flex flex-col hover:shadow-md hover:shadow-gray-300 h-[400px] pt-10 items-center w-[300px]">
-      <Image src={imageUrl} className="pb-6" alt="user-pic" />
+    <div
+      id="card1"
+      className="flex flex-col hover:shadow-md hover:shadow-gray-300 h-[400px] pt-10 items-center w-[300px]"
+    >
+      <Image src={`${imageUrl}`} className="pb-6" alt="user-pic" />
       <h1 className="text-2xl pb-3 font-bold">{title}</h1>
-      <p className="text-center pb-3 text-[#82847e]">
-        {desc}
-      </p>
-      <Link href={linkurl}  className="flex gap-1 text-red-500 items-center font-bold">Read More <FaChevronRight /></Link>
+      <p className="text-center pb-3 text-[#82847e]">{desc}</p>
+      <Link
+        href={`${linkurl}`}
+        className="flex gap-1 text-red-500 items-center font-bold"
+      >
+        Read More <FaChevronRight />
+      </Link>
     </div>
   );
 };
