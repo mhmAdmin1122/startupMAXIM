bd5bd6d76a16842ff234540a0b4c4da303bddae1
imranahmadlashari <151011407+imranahmadlashari@users.noreply.github.com>
2024-02-03 16:01:27 +0500

    Error removed

--- pages/Components/H/HomeSection.tsx
+++ pages/Components/H/HomeSection.tsx
@@ -46,7 +46,7 @@ const HomeSection = () => {
             <br />StatupMAXIM helps you generate results at every stage of your startup, from ideation to MVP, facilitating seamless scaling towards a successful exit, 
             to elevating your brand to new heights, StartupMAXIM offers tailored solutions for every stage of your entrepreneurial journey. 
             <br /><strong>Worlds first dedicated marketing agency for startups.</strong>
-          </p>
+          
           <button class="p-4 pl-6 max-[385px]:mb-4 pr-6  bg-[#fa6742] hover:bg-[#2e2c2d] text-white font-bold rounded-full mr-5 mt-5">LEARN MORE</button>
         </div>
       </section>
