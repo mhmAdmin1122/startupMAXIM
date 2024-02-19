0d3a2234578c3bebed58ff71f5f03bca49162d6d
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-02-20 02:27:59 +0500

    few more changing

--- pages/Blog/index.tsx
+++ pages/Blog/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import Layout from '../Components/L/Layout'
-import Head from 'next/head'
-import Homecard from '../Components/H/HomeCard'
+import React from "react";
+import Layout from "../Components/L/Layout";
+import Head from "next/head";
+import Homecard from "../Components/H/HomeCard";
 import post1 from "@/public/img/post1.jpg";
 import post2 from "@/public/img/post2.jpg";
 import post3 from "@/public/img/post3.jpg";
+import Marque from "../Components/M/Marque";
 
 const BlogPage = () => {
   return (
@@ -12,16 +13,19 @@ const BlogPage = () => {
       <Head>
         <title>Blog - StartupMAXIM</title>
       </Head>
-      <div className='py-10 text-[45px] font-bold text-center'>
-        <h1>Read Our <b className='text-[#ff6e4e]'>Blog</b></h1>
+      <div className="py-10 text-[45px] font-bold text-center flex flex-col justify-center items-center gap-2">
+        <h1>
+          Read Our <b className="text-[#ff6e4e]">Blog</b>
+        </h1>
+        <Marque />
       </div>
-      <div className='flex flex-wrap items-center gap-10 justify-center px-[25px] pb-[100px] text-wrap w-full'>
+      <div className="flex flex-wrap items-center gap-10 justify-center px-[25px] pb-[100px] text-wrap w-full">
         <Homecard imageUrl={post1} />
         <Homecard imageUrl={post2} />
         <Homecard imageUrl={post3} />
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
